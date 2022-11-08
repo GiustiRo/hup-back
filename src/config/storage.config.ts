be194bd91e91ff026adcada37c644658698cd2eb
rgiusti <rgiusti@alabamasolutions.com>
2022-11-08 08:46:08 -0300

    updated typescript fixed types errors.

--- src/config/storage.config.ts
+++ src/config/storage.config.ts
@@ -1,10 +1,10 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('gcs', () => ({
-    projectId: process.env.PROJECT_ID,
-    private_key: process.env.PRIVATE_KEY,
-    client_email: process.env.CLIENT_EMAIL,
-    gcsBucket_AVATARS: process.env.STORAGE_MEDIA_BUCKET_AVATARS,
+    projectId: process.env.GCS_PROJECT_ID,
+    private_key: process.env.GCS_PRIVATE_KEY,
+    client_email: process.env.GCS_CLIENT_EMAIL,
+    gcsBucket_AVATARS: process.env.GCS_STORAGE_MEDIA_BUCKET_AVATARS,
 }));
 
 
